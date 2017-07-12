1d39cedf64596d0258857ee77407bd012918ddc4
Alexey Toklovich <alexey@toklovich.com>
2017-07-13 00:04:33 +0700

    Additional menus for copy-paste

--- index.js
+++ index.js
@@ -1,10 +1,116 @@
 const electron = require('electron');
 const app = electron.app;
+const Menu = electron.Menu;
 const dialog = electron.dialog;
 const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 let mainWindow;
+
+let template = [
+    {
+        label: 'NightOut Photo Downloader',
+        submenu: [
+            {
+                label: 'Hide NightOut Photo Downloader',
+                accelerator: 'Command+H',
+                selector: 'hide:'
+            },
+            {
+                label: 'Hide Others',
+                accelerator: 'Command+Shift+H',
+                selector: 'hideOtherApplications:'
+            },
+            {
+                label: 'Show All',
+                selector: 'unhideAllApplications:'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Quit',
+                accelerator: 'Command+Q',
+                click: function() { app.quit(); }
+            },
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Undo',
+                accelerator: 'Command+Z',
+                selector: 'undo:'
+            },
+            {
+                label: 'Redo',
+                accelerator: 'Shift+Command+Z',
+                selector: 'redo:'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Cut',
+                accelerator: 'Command+X',
+                selector: 'cut:'
+            },
+            {
+                label: 'Copy',
+                accelerator: 'Command+C',
+                selector: 'copy:'
+            },
+            {
+                label: 'Paste',
+                accelerator: 'Command+V',
+                selector: 'paste:'
+            },
+            {
+                label: 'Select All',
+                accelerator: 'Command+A',
+                selector: 'selectAll:'
+            },
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {
+                label: 'Reload',
+                accelerator: 'Command+R',
+                click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); }
+            },
+            {
+                label: 'Toggle DevTools',
+                accelerator: 'Alt+Command+I',
+                click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); }
+            },
+        ]
+    },
+    {
+        label: 'Window',
+        submenu: [
+            {
+                label: 'Minimize',
+                accelerator: 'Command+M',
+                selector: 'performMiniaturize:'
+            },
+            {
+                label: 'Close',
+                accelerator: 'Command+W',
+                selector: 'performClose:'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Bring All to Front',
+                selector: 'arrangeInFront:'
+            },
+        ]
+    },
+];
 
 function createWindow () {
     mainWindow = new BrowserWindow({width: 800, height: 600});
@@ -20,6 +126,9 @@ function createWindow () {
     mainWindow.on('closed', function () {
         mainWindow = null;
     });
+
+    menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
 }
 
 app.on('ready', createWindow);
